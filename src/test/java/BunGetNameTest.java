a6019d23c61df2544e78134762c75754ebea3c2f
Ekaterina Zarubina <edubovenko@gmail.com>
2022-03-22 18:53:53 +0700

    initial commit

---
+++ src/test/java/BunGetNameTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import org.mockito.Mock;
+import praktikum.Bun;
+
+public class BunGetNameTest {
+
+    @Test
+    public void BunNameIs(){
+
+
+    }
+}
---
+++ src/test/java/IngredientTypeTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import praktikum.IngredientType;
+
+
+public class IngredientTypeTest {
+
+    @Test
+    // метод принимает на вход тип ингридиента
+    //если это SAUCE или FILLING - вернет true
+    public boolean isIngredientPresent (){
+
+    }
+}
