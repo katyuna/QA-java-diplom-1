6973538f9465fc8095f9fbd8b04717cb7c258fd0
Ekaterina Zarubina <edubovenko@gmail.com>
2022-03-23 13:30:50 +0700

    BunTest

---
+++ src/test/java/BunTest.java
@@ -0,0 +1,41 @@
+import com.github.javafaker.Faker;
+import org.junit.Test;
+import praktikum.Bun;
+
+import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
+
+public class BunTest {
+
+    //ARRANGE
+    //Сгенерировать тестовые данные - имя и цену булочки
+    Faker faker = new Faker();
+    Random random = new Random();
+    String name = faker.food().toString();
+    Float price = random.nextFloat();
+
+    //ACT
+    //Создать объект Bun
+    Bun bun = new Bun(name, price);
+
+    @Test
+    public void bunNameIs() {
+        //Вызвать тестируемый метод
+        String actual = bun.getName();
+        String expected = name;
+        //ASSERT
+        //Проверить, что метод вызван с заданными параметрами
+        assertEquals("Ошибка в работе метода bun.getName()", expected, actual);
+    }
+
+    @Test
+    public void bunPriceIs() {
+        //Вызвать тестируемый метод
+        float actual = bun.getPrice();
+        float expected = price;
+        //ASSERT
+        //Проверить, что метод вызван с заданными параметрами
+        assertEquals("Ошибка в работе метода bun.getPrice()", expected, actual, 0);
+    }
+}
